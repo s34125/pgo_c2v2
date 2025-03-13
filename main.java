f4007ea5bd11a60612c2733f2a0d852f2424732f
s34125 <s34125@pjwstk.edu.pl>
2025-03-13 10:47:17 +0100

    zad1

--- main.java
+++ main.java
@@ -1,5 +1,22 @@
 public class main { new*
     public static void main(String[] args) {
+        public static int[] filterEvenIndexOddValue(int[] array) {
+            int count = 0;
+            for (int i = 0; i < array.length; i += 2) {
+                if (array[i] % 2 != 0) {
+                    count++;
+                }
+            }
 
+            int[] result = new int[count];
+            int index = 0;
+            for (int i = 0; i < array.length; i += 2) {
+                if (array[i] % 2 != 0) {
+                    result[index++] = array[i];
+                }
+            }
+
+            return result;
+        }
     }
 }
